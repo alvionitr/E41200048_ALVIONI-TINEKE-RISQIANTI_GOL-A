39e96bc579554d6855e3423db488a00db90493b2
ALVIONI TINEKE RISQIANTI <80692071+alvionitr@users.noreply.github.com>
2021-03-16 20:00:25 +0700

    Minggu 2

---
+++ Tugas Minggu2/Tugas a.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas.no.pkg5;
+
+/**
+ *
+ * @author Asus
+ */
+public class Person {
+String fName = "Alvioni";
+        String lName = "Risqianti";
+        int stuId = 41200048;
+        String stuStatus = "Active";
+        
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String fName = "Alvioni";
+        String lName = "Risqianti";
+        int stuId = 41200048;
+        String stuStatus = "Active";
+        
+        System.out.println("Student Name: " + fName +" "+ lName );
+        System.out.println ("Student ID: " + stuId);
+        System.out.println ("Student Status: " + stuStatus);
+    }
+    
+}
